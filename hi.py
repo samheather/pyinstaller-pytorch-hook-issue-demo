50516eaf23870915fedc7788c40a6624a9453169
samheather <sam@heather.sh>
2022-10-10 16:37:17 +0100

    Initial commit

---
+++ hi.py
@@ -0,0 +1,7 @@
+from imaginairy import imagine, imagine_image_files, ImaginePrompt, WeightedPrompt, LazyLoadingImage
+
+prompts = [
+    ImaginePrompt("a bowl of fruit")
+]
+for result in imagine(prompts):
+    result.save("my_image.jpg")
